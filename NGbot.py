4f8eca3c576bd720ac3ed1ec84ba2ce62c598da7
sssssan <sssssan@gmail.com>
2023-09-20 12:23:17 +0800

    Update NGbot.py

--- NGbot.py
+++ NGbot.py
@@ -2,7 +2,8 @@ import streamlit as st
 from llama_index import VectorStoreIndex, ServiceContext, Document
 from llama_index.llms import OpenAI
 import openai
-from llama_index import SimpleDirectoryReader
+from llama_index import SimpleDirectoryReader, StorageContext, load_index_from_storage
+
 
 st.set_page_config(page_title="Chat with Nutri-Grade policy documents (powered by LlamaIndex)", page_icon="", layout="centered", initial_sidebar_state="auto", menu_items=None)
 openai.api_key = st.secrets.openai_key
@@ -18,15 +19,19 @@ if "messages" not in st.session_state.keys(): # Initialize the chat messages his
 @st.cache_resource(show_spinner=False)
 def load_data():
     with st.spinner(text="Loading and indexing the Nutri-Grade docs – hang tight! This should take 1-2 minutes."):
-        reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
-        docs = reader.load_data()
+        
+        # Rebuild the storage context
+        storage_context = StorageContext.from_defaults(persist_dir="./data/index.vecstore")
+
+        # Load the index
+        index = load_index_from_storage(storage_context)
+
+        # Load the finetuned model 
         ft_model_name = "ft:gpt-3.5-turbo-0613:personal::80aOEYbP"
         ft_context = ServiceContext.from_defaults(llm=OpenAI(model=ft_model_name, temperature=0.2), context_window=2048, system_prompt="You are an expert on the Nutri-Grade documents and policy and your job is to answer questions related to the policy. Assume that all questions are related to the Nutri-Grade policy. Keep your answers technical and based on facts – do not hallucinate features.")
         index = VectorStoreIndex.from_documents(docs, service_context=ft_context)
         return index
 
-index = load_data()
-# chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True, system_prompt="You are an expert on the Streamlit Python library and your job is to answer technical questions. Assume that all questions are related to the Streamlit Python library. Keep your answers technical and based on facts – do not hallucinate features.")
 chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
 
 if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
